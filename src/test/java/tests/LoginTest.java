57526236c083b468664599d546315951bf1f475a
sudheer04k-ai <sudheer.parlapati@gmail.com>
2026-09-09 17:58:56 +0530

    Initial SDET CI/CD project setup

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void validLoginTest() {
+
+        System.out.println("Executing valid login test");
+
+        String actualResult = "Login Successful";
+        String expectedResult = "Login Successful";
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        System.out.println("Login test passed");
+    }
+}
---
+++ src/test/java/tests/SmokeTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void verifyApplication() {
+        System.out.println("Application is working!");
+    }
+}
